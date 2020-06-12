201e78b8215a4351e1c9959f66bd0c62e5314021
impactlearner <impactlearner2020@gmail.com>
2020-06-12 08:55:16 -0400

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Write your code below game_hash
 def game_hash
   {
@@ -127,3 +128,116 @@ def game_hash
 end
 
 # Write code here
+
+def all_players
+  #for the entire hash, create a collection of all players on both teams
+   game_hash[:home][:players].merge(game_hash[:away][:players])
+   players = game_hash.keys.map do |team|
+     game_hash[team][:players]
+ end
+# binding.pry
+ players.flatten
+end
+
+
+def num_points_scored(target_player)
+  #access players from both home and away
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:player_name] == target_player
+        return player[:points]
+      end
+    end
+  end
+end
+
+
+def shoe_size(target_player)
+  #get shoe size for all players
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:player_name] == target_player
+        return player[:shoe]
+      end
+    end
+  end
+end
+
+def team_colors(location)
+  #shows correct team colors for both home and away locations
+  if location == "Brooklyn Nets"
+    return game_hash[:home][:colors]
+  else return game_hash[:away][:colors]
+  end
+end
+
+def team_names
+  #returns each team name
+  game_hash.map do |team, team_info|
+    team_info[:team_name]
+  end
+end
+
+def player_numbers(input)
+  #returns the player jersey numbers
+  output = []
+  game_hash.each do |team, team_info|
+    if team_info[:team_name] == input
+      team_info.each do |key, value|
+        if key == :players
+          value.each do |player|
+            output.push(player[:number])
+          end
+        end
+      end
+    end
+  end
+  return output
+end
+
+def player_stats(input)
+  #returns all stats for target player
+  game_hash.each do |team, team_info|
+    team_info.each do |key, value|
+        if key == :players
+          value.each do |player|
+              if input == player[:player_name]
+                  return player
+              end
+            end
+          end
+        end
+      end
+    end
+
+def big_shoe_rebounds
+  #returns number of rebounds for player with biggest shoe
+  biggest_shoe = 0
+  rebounds = 0
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:shoe] > biggest_shoe
+        biggest_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  return rebounds
+end
+# def num_points_scored(player_name)
+#   game_hash[:home][:players][:player_name]
+  
+  #binding.pry
+  #iterate through game_hash to find player name
+    #break down this process
+  #return the points associated with player name
+    #break down this process
+# game_hash.values.map do |team_data|
+#     team_data[:players]
+#   end.flatten
+# end
+#game_hash.each do |location, team_data|
+  #get the players
+  #player = team_data[:players].select do |player|
+#num_points_scored("Jeff Adrien")
+
